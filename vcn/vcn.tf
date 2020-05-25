295e0432f972583a0a6cce2a02d2b9576df39b69
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-25 13:23:24 +0200

    Update vcn.tf

--- vcn/vcn.tf
+++ vcn/vcn.tf
@@ -9,23 +9,3 @@ resource "oci_core_vcn" "vcn" {
     dns_label = var.vcn_dns_label
 
 }
-
-# Creates a NAT-Gateway if requested
-resource "oci_core_nat_gateway" "nat_gateway" {
-    count = var.create_nat_gateway == true ? 1 : 0
-    
-    # Requried
-    compartment_id = var.compartment_id
-    vcn_id = oci_core_vcn.vcn.id
-
-    # Optional
-    display_name = var.nat_gateway_display_name
-}
-
-
-
-# Create a Service Gateway if requested
-
-
-
-
