59a19d2ae9f5e59b22791b22fe8200fd745d6d9b
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-25 15:09:49 +0200

    Update output.tf

--- vcn/output.tf
+++ vcn/output.tf
@@ -2,3 +2,24 @@ output "vcn_OCID" {
     description = "OCID of the created VCN"
     value = oci_core_vcn.vcn.id
 }
+
+output "nat_gateway_OCID" {
+    description = "OCID of the created NAT Gateway"
+    oci_core_nat_gateway.nat_gateway.id
+} 
+
+output "nat_gateway_IP" {
+    description = "IP address of the NAT Gateway"
+    oci_core_nat_gateway.nat_gateway.nat_id
+}
+
+output "internet_gateway_OCID" {
+    description = "OCID of the created NAT Gateway"
+    oci_core_nat_gateway.internet_gateway.id
+} 
+
+output "drg_OCID" {
+    description = "OCID of the created Dynamic Routing Gateway"
+    oci_core_drg.dynamic_routing_gateway.id
+} 
+
