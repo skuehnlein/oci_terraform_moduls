ae358e4ca57f0b11f61f0dee9cdd696999bce57d
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-25 13:19:13 +0200

    Update drg.tf

--- vcn/drg.tf
+++ vcn/drg.tf
@@ -1,6 +1,6 @@
 # Create a Dynamic Routing Gateway if requested
 resource "oci_core_drg" "dynamic_routing_gateway" {
-    count = var.create_dynamic_routing_gateway = true ? 1 : 0
+    count = var.create_dynamic_routing_gateway == true ? 1 : 0
 
     #Required
     compartment_id = var.compartment_id
