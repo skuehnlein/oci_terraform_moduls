09829b16960a362f441c9bdf52e05fba64ce7d29
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-27 10:23:15 +0200

    Update iam_user.tf

--- iam/iam_user.tf
+++ iam/iam_user.tf
@@ -1,11 +1,9 @@
 # Creates all requiered users
 resource "oci_identity_user" "user" {
-    name = "User"
     compartment_id = var.tenancy_OCID
-    description = "OCI IAM User for "
 
     dynamic "user" {
-        for_each var.iam_users
+        for_each = var.iam_users
         content {
             name = user.value["user_name"]
             description = "OCI IAM User for "
