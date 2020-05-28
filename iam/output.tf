3dfcce81fd86c50365b9f81a22dffa129863ddd4
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-28 12:17:21 +0200

    Update output.tf

--- iam/output.tf
+++ iam/output.tf
@@ -1,10 +1,10 @@
 output "iam_users" {
     description = "created users in the tenancy"
-    value = zipmap(list(oci_identity_user.users.*.name), list(oci_identity_user.users.*.id))
+    value = zipmap(list(oci_identity_user.users))
   
 }
 
 output "iam_groups" {
     description = "created groups in the tenancy"
-    value = zipmap(list(oci_identity_group.groups.*.name), list(oci_identity_group.groups.*.id))
+    value = zipmap(list(oci_identity_group.groups))
 }
