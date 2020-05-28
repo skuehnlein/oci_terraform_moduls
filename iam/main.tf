29e22508c0060cf04e8fdf2cc2b91474790d44e3
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-28 11:50:10 +0200

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -33,6 +33,6 @@ resource "oci_identity_group" "groups" {
 resource "oci_identity_user_group_membership" "membership" {
     for_each = var.iam_users_group_membership
 
-    user_id = lookup(oci_identity_user.users.user.name, each.value["user_name"]).id
+    user_id = lookup(oci_identity_user.users[each.value], each.value["user_name"]).id
     group_id  = lookup(oci_identity_group.groups[each.value], each.value["group_name"]).id
 }
