ca12067851848e788f33ed2fd6d356c75f548569
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-28 13:06:10 +0200

    Update output.tf

--- iam/output.tf
+++ iam/output.tf
@@ -8,7 +8,7 @@ output "iam_users" {
 
 output "iam_groups" {
     description = "created groups in the tenancy"
-    value = values(oci_identity_user.users)[*].user.name
+    value = values(oci_identity_user.users)[*].name
 #    value = {
 #        for user in oci_identity_user.users:
 #        user.name => user.id
