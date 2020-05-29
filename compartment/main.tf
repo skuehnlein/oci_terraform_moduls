79aa6873a8ee55f28faa95355f6b6941bc71fb33
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-29 17:51:13 +0200

    Update main.tf

--- compartment/main.tf
+++ compartment/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.12.24"
 
-  backend "s3" {}
+#  backend "s3" {}
 
   required_providers {
         oci = "~> 3.75"
@@ -21,12 +21,17 @@ output "oci_compartments" {
     value = data.oci_identity_compartments.all_compartments
 }
 
+resource "oci_identity_compartment" "compartment" {
+    compartment_id = var.tenancy_OCID
+    description = "test"
+    name = "test"
+}
 #resource "oci_identity_compartment" "compartment" {
 
 #    for_each = var.compartments
 
     # Required
 #    compartment_id = lookup(zipmap(data.oci_identity_compartments.all_compartments.compartments[*].name, data.oci_identity_compartments.all_compartments.compartments[*].id),each.value["root_compartment"])
-#    description = each.value["compartment_description"]
-#    name = each.value["compartment_name"]
+ #   description = each.value["compartment_description"]
+  #  name = each.value["compartment_name"]
 #}
