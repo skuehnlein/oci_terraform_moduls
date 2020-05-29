2dcd0e04cae5eb8678cdd2e2d74b9de0940eca72
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-29 11:32:15 +0200

    Update output.tf

--- compartment/output.tf
+++ compartment/output.tf
@@ -1,9 +1,4 @@
 # general parameters
-variable "tenancy_OCID" {
-    description = "OCID of the tenancy"
-    type = string
-}
-
 variable "compartments" {
     description = "Created Compartments"
     value = zipmap(values(oci_identity_compartments.compartments)[*].name,values(oci_identity_compartments.compartments)[*].id)
