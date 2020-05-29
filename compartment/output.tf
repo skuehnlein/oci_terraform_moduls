090d73ea96f334203676ad1821e046313aab626e
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-29 11:36:02 +0200

    Update output.tf

--- compartment/output.tf
+++ compartment/output.tf
@@ -1,5 +1,5 @@
 # general parameters
-variable "compartments" {
+variable "created_compartments" {
     description = "Created Compartments"
     value = zipmap(values(oci_identity_compartments.compartments)[*].name,values(oci_identity_compartments.compartments)[*].id)
 }
