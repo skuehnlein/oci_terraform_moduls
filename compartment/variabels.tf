eafee50262b637e866f0000f0ca4b951134280af
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-05-31 12:03:27 +0200

    Update variabels.tf

--- compartment/variabels.tf
+++ compartment/variabels.tf
@@ -13,6 +13,5 @@ variable "compartments" {
 
 variable "is_top_level_compartemt" {
     description = "Flag that indicats, that the compartment should be created als child of the root compartment"
-    type = bool
     default = true
 }
