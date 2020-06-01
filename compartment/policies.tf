18ccb9ea9905afa2bc44e2990971767a6efc5bdd
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-01 09:01:56 +0200

    Update policies.tf

--- compartment/policies.tf
+++ compartment/policies.tf
@@ -8,14 +8,14 @@ data "oci_identity_compartments" "compartments" {
 }
 
 locals {
-    compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
+    all_compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
 }
 
 resource "oci_identity_policy" "compartment_policies" {
     for_each = var.all_compartment_policies
 
     #Required
-    compartment_id = lookup(local.compartment_ids,each_value["compartment_name"])
+    compartment_id = lookup(local.all_compartment_ids,each_value["compartment_name"])
     name = each_value["name"]
     description = each_value["description"]
     statements = each_value["statements"]
