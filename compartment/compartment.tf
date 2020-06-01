530d0a1c1d5b2149a1b566f94cb0940c1ed3f459
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-01 08:22:03 +0200

    compartments

---
+++ compartment/compartment.tf
@@ -0,0 +1,22 @@
+data "oci_identity_compartments" "compartments" {
+    # Required
+    compartment_id = var.tenancy_OCID
+
+    # Optional
+    access_level = "ACCESSIBLE"
+    compartment_id_in_subtree = true
+}
+
+locals {
+    compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
+}
+
+resource "oci_identity_compartment" "compartment" {
+
+    #for_each = var.compartments
+    count = length(var.compartments)
+    # Required
+    compartment_id = var.is_top_level_compartemt ? var.tenancy_OCID : lookup(local.compartment_ids,var.compartments[*].root_compartment)
+    description = var.compartments[*].compartment_description
+    name = var.compartments[*].compartment_name
+}
--- compartment/main.tf
+++ compartment/main.tf
@@ -8,25 +8,3 @@ terraform {
     }
 }
 
-data "oci_identity_compartments" "compartments" {
-    # Required
-    compartment_id = var.tenancy_OCID
-
-    # Optional
-    access_level = "ACCESSIBLE"
-    compartment_id_in_subtree = true
-}
-
-locals {
-    compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
-}
-
-resource "oci_identity_compartment" "compartment" {
-
-    for_each = var.compartments
-
-    # Required
-    compartment_id = var.is_top_level_compartemt ? var.tenancy_OCID : lookup(local.compartment_ids,each.value["root_compartment"])
-    description = each.value["compartment_description"]
-    name = each.value["compartment_name"]
-}
