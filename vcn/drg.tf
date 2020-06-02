4b9c00a83e78706a9b1b69d9259c5768ba3a0a05
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-02 14:48:56 +0200

    Update drg.tf

--- vcn/drg.tf
+++ vcn/drg.tf
@@ -9,24 +9,24 @@ resource "oci_core_drg" "dynamic_routing_gateway" {
     display_name = var.drg_display_name
 }
 
-resource "oci_core_route_table" "drg_route_table" {
-    count = var.create_dynamic_routing_gateway == true ? 1 : 0
+#resource "oci_core_route_table" "drg_route_table" {
+#    count = var.create_dynamic_routing_gateway == true ? 1 : 0
 
     #Required
-    compartment_id =  lookup(local.compartment_ids,var.compartment_name)
-    vcn_id = oci_core_vcn.vcn.id
+#    compartment_id =  lookup(local.compartment_ids,var.compartment_name)
+#    vcn_id = oci_core_vcn.vcn.id
 
     #Optional
-    display_name = "Route Table for the Dynamic Routing Gateway"
-    route_rules{
+#    display_name = "Route Table for the Dynamic Routing Gateway"
+#    route_rules{
         # Required
-        network_entity_id = oci_core_drg.dynamic_routing_gateway[0].id
+#        network_entity_id = oci_core_drg.dynamic_routing_gateway[0].id
 
         #Optionally
-        destination = "0.0.0.0/0"
-        destination_type = "CIDR_BLOCK"
-    }
-}
+#        destination = "0.0.0.0/0"
+#        destination_type = "CIDR_BLOCK"
+#    }
+#}
 
 resource "oci_core_drg_attachment" "drg_attachment" {
     count = var.create_dynamic_routing_gateway == true ? 1 : 0
