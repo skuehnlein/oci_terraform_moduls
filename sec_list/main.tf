4b3ffa3452e47ba6f387b1c916daecf1e28db3a5
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-03 06:35:07 +0200

    weitere Attribute

--- sec_list/main.tf
+++ sec_list/main.tf
@@ -21,9 +21,6 @@ locals {
     compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
 }
 
-
-
-
 resource "oci_core_security_list" "security_lists" {
 
     # Required
@@ -34,34 +31,105 @@ resource "oci_core_security_list" "security_lists" {
     display_name = var.security_list_display_name
     
     dynamic "egress_security_rules" {
-        for_each = var.egress_rules
+        iterator = tcp_egress_iterator
+        for_each = var.tcp_egress_rules
+
         content {
-            destination = egress_security_rules.value["destination"]
+            description = tcp_egress_iterator.value["description"]
+            destination = tcp_egress_iterator.value["destination"]
+            destination_type = tcp_egress_iterator.value["destination_type"]
             protocol =  6
+        }
+
+        dynamic "tcp_options" {
+            for_each = tcp_egress_iterator.value["destination_port"] == -1 ? [] : list[1]
+            content{
+                min = tcp_egress_iterator.value["destination_port"]
+                max = tcp_egress_iterator.value["destination_port"]
+            }
+        }
+    }
+
+    dynamic "egress_security_rules" {
+        iterator = icmp_egress_iterator
+        for_each = var.icmp_egress_rules
+
+        content {
+            description = icmp_egress_iterator.value["description"]
+            destination = icmp_egress_iterator.value["destination"]
+            destination_type = icmp_egress_iterator.value["destination_type"]
+            protocol =  1
+        }
+    }
+
+    dynamic "egress_security_rules" {
+        iterator = udp_egress_iterator
+        for_each = var.udp_egress_rules
+
+        content {
+            description = udp_egress_iterator.value["description"]
+            destination = udp_egress_iterator.value["destination"]
+            destination_type = udp_egress_iterator.value["destination_type"]
+            protocol =  17
+        }
+
+        dynamic "udp_options" {
+            for_each = udp_egress_iterator.value["destination_port"] == -1 ? [] : list[1]
+            content{
+                min = udp_egress_iterator.value["destination_port"]
+                max = udp_egress_iterator.value["destination_port"]
+            }
         }
     }
 
     dynamic "ingress_security_rules" {
+        iterator = tcp_ingress_iterator
         for_each = var.tcp_ingress_rules
+
         content {
-            source = ingress_security_rules.value["source"]
+            description = tcp_ingress_iterator.value["description"]
+            source = tcp_ingress_iterator.value["source"]
+            source_type = tcp_ingress_iterator.value["source_type"]
             protocol =  6
+        }
+
+        dynamic "tcp_options" {
+            for_each = tcp_ingress_iterator.value["destination_port"] == -1 ? [] : list[1]
+            content{
+                min = tcp_ingress_iterator.value["destination_port"]
+                max = tcp_ingress_iterator.value["destination_port"]
+            }
         }
     }
 
     dynamic "ingress_security_rules" {
+        iterator = icmp_ingress_iterator
         for_each = var.icmp_ingress_rules
         content {
-            source = ingress_security_rules.value["source"]
+            description = icmp_ingress_iterator.value["description"]
+            source = icmp_ingress_iterator.value["source"]
+            source_type = icmp_ingress_iterator.value["source_type"]
             protocol =  1
         }
     }
 
     dynamic "ingress_security_rules" {
+        iterator = udp_ingress_iterator
         for_each = var.udp_ingress_rules
+
         content {
-            source = ingress_security_rules.value["source"]
+            description = udp_ingress_iterator.value["description"]
+            source = udp_ingress_iterator.value["source"]
+            source_type = udp_ingress_iterator.value["source_type"]
             protocol =  17
+        }
+
+        dynamic "udp_options" {
+            for_each = udp_ingress_iterator.value["destination_port"] == -1 ? [] : list[1]
+            content{
+                min = udp_ingress_iterator.value["destination_port"]
+                max = udp_ingress_iterator.value["destination_port"]
+            }
         }
     }
 
--- sec_list/variables.tf
+++ sec_list/variables.tf
@@ -21,31 +21,73 @@ variable "security_list_display_name" {
 variable "tcp_ingress_rules" {
     description = "List of all ingress rules of the security list - protocol: TCP"
     type = list(object({
-        source =  string
-
+        description = string,
+        source =  string,
+        source_type = string,
+        destination_port = number,
+        stateless = bool
     }))
+    default = []
 }
 
 variable "icmp_ingress_rules" {
     description = "List of all ingress rules of the security list - protocol: ICMP"
     type = list(object({
-        source =  string
-
+        description = string,
+        source =  string,
+        source_type = string,
+        stateless = bool
     }))
+    default = []
 }
 
 variable "udp_ingress_rules" {
-    description = "List of all ingress rules of the security list - protocol: ICMP"
+    description = "List of all ingress rules of the security list - protocol: UDP"
     type = list(object({
-        source =  string
+        description = string,
+        source =  string,
+        source_type = string,
+        destination_port = number,
+        stateless = bool
 
     }))
+    default = []
 }
 
-variable "egress_rules" {
-    description = "List of all egress rules of the security list"
+variable "tcp_egress_rules" {
+    description = "List of all egress rules of the security list - protocol: TCP"
     type = list(object({
-        destination = string
+        description = string,
+        destination = string,
+        destination_type = string
+        destination_port = number,
+        stateless = bool
 
     }))
+    default = []
+}
+
+variable "icmp_egress_rules" {
+    description = "List of all egress rules of the security list - protocol: ICMP"
+    type = list(object({
+        description = string,
+        destination = string,
+        destionation_type = string
+        stateless = bool
+
+    }))
+    default = []
+}
+
+variable "udp_egress_rules" {
+    description = "List of all egress rules of the security list - protocol: UDP"
+    type = list(object({
+        description = string,
+        destination = string,
+        destination_type = string,
+        destination_port = number,
+        stateless = bool
+
+    }))
+    default = []
 }
