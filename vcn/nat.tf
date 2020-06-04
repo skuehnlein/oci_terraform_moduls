c9a1c68040272e2ef709d7cf116bb5ec9d3e2919
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-04 10:20:04 +0200

    rework routingtable

--- vcn/nat.tf
+++ vcn/nat.tf
@@ -10,22 +10,4 @@ resource "oci_core_nat_gateway" "nat_gateway" {
     display_name = var.nat_gateway_display_name
 }
 
-resource "oci_core_route_table" "nat_gateway_route_table" {
-    count = var.create_nat_gateway == true ? 1 : 0
-
-    #Required
-    compartment_id =  lookup(local.compartment_ids,var.compartment_name)
-    vcn_id = oci_core_vcn.vcn.id
-
-    #Optional
-    display_name = "Route Table for the NAT Gateway"
-    route_rules {
-        # Required
-        network_entity_id = oci_core_nat_gateway.nat_gateway[0].id
-
-        #Optionally
-        destination = "0.0.0.0/0"
-        destination_type = "CIDR_BLOCK"
-    }
-}
 
