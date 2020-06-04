c0956a2a50a4afa194fb8d981f2a9291de5f5516
Stefan Kuehnlein <stefan.kuehnlein@opitz-consulting.com>
2020-06-04 10:43:26 +0200

    route table

---
+++ route_table/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 0.12.24"
+
+#  backend "s3" {}
+
+  required_providers {
+        oci = "~> 3.75"
+    }
+}
+
+data "oci_identity_compartments" "compartments" {
+    # Required
+    compartment_id = var.tenancy_OCID
+
+    # Optional
+    access_level = "ACCESSIBLE"
+    compartment_id_in_subtree = true
+}
+
+locals {
+    compartment_ids = zipmap(data.oci_identity_compartments.compartments.compartments[*].name, data.oci_identity_compartments.compartments.compartments[*].id)
+}
+
+resource "oci_core_route_table" "route_table" {
+    #Required
+    vcn_id = var.vcn_OCID
+    compartment_id = lookup(local.compartment_ids,var.compartment_name)
+
+    #Optional
+    display_name = var.route_table_display_name
+
+    dynamic "route_rules" {
+        for_each = var.route_rules
+
+        content {
+            # Required
+            network_entity_id = route_rules.value["network_entity_id"
+
+            #Optional
+            cidr_block = route_rules.value["cidr_block"]
+            description = route_rules.value["description"]
+            destination = route_rules.value["destination"]
+            destionation_type = route_rules.value["destination_type"]
+        }
+    }
+
+}
---
+++ route_table/variables.tf
@@ -0,0 +1,27 @@
+variable "compartment_name" {
+    description = "Name of the compartment in which the resource will be created"
+    type = string
+}
+
+variable "route_table_display_name" {
+    description = "The name of the route table which will be shown in the OCI console"
+    type = string
+    default = null
+}
+
+variable "vcn_OCID" {
+    description = "The OCID of the vcn"
+    type = string
+}
+
+
+variable "route_rules" {
+    description = "Route Rules for the route table"
+    type = list(object({
+        network_entity_id = string,
+        cidr_block = string,
+        description = string,
+        destination = string,
+        destination_type = string
+    }))
+} 
